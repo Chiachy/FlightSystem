150214e8e8d19d60ad454e4896ea5c77201e7025
shakuganns <z18679760733@gmail.com>
2016-04-03 12:48:00 +0800

    添加订单，用户数据库

---
+++ app/src/main/java/com/shanshan/flightmanager/UserDatas.java
@@ -0,0 +1,63 @@
+package com.shanshan.flightmanager;
+
+
+/**
+ * Created by Shakugan on 16/4/3.
+ */
+public class UserDatas {
+
+    private int id;
+    private String password;
+    private String name;
+    private String sex;
+    private int age;
+    private int balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
